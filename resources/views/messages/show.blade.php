dbe380b984133951b8e52bd36c33130927ad9b58
Yasmanis Basulto <yasmabc@gmail.com>
2019-08-30 16:40:10 -0400

    vista show termina

--- resources/views/messages/show.blade.php
+++ resources/views/messages/show.blade.php
@@ -1,12 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Mensaje</h1>
-    <p>{{$message->body}}</p>
-    <small>Enviado por {{$message->sender->name}}</small>
-    <form method="POST" action="{{route('response_message')}}">
-        @csrf
-        <input type="hidden" name="user_response" value="{{$message->sender_id}}">
-        <input type="submit" value="Responder">
-    </form>
+
+<div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Mensaje Enviado por {{$message->sender->name}}</div>
+    
+                    <div class="card-body">
+                        <h5 class="text-center">Contenido del mensaje</h5>
+                        <hr>
+                            <p>{{$message->body}}</p>
+                    </div>
+                </div>
+                <form method="POST" action="{{route('response_message')}}">
+                        @csrf
+                        <input type="hidden" name="user_response" value="{{$message->sender_id}}">
+                        <input class="btn btn-primary float-right" type="submit" value="Responder">
+                </form>
+            </div>
+        </div>
+    </div>
 @endsection
